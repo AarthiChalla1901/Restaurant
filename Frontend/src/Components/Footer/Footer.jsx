46694940a95d406bb710035b2006f7c0be7f3767
aarthireddychalla <n191090@rguktn.ac.in>
2024-12-05 12:43:32 +0530

    The files are commited

---
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import "./Footer.css"
+import { assets } from '../../assets/assets'
+const Footer = () => {
+  return (
+        <div className="footer" id='footer'>
+            <div className="footer-contents">
+                <div className="footer-content-left">
+                    <img className='logo' src={assets.logo_new} alt="" />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum magni voluptatem dolores ea consequuntur</p>
+                    <div className="social-icons">
+                        <img src={assets.facebook_icon1} alt="" />
+                        <img src={assets.twitter_icon1} alt="" />
+                        <img src={assets.linkedin_icon1} alt="" />
+                    </div>
+                </div>
+                <div className="footer-content-center">
+                    <h2>Company</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Delivery</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+                <div className="footer-content-right">
+                    <h2>GET IN TOUCH</h2>
+                    <ul>
+                        <li>+91-432-567-89</li>
+                        <li>contact@cravecurb.com</li>
+                    </ul>
+                </div>
+            </div>
+            <hr/>
+        <p className='footer-copy-right'>Copy Right © 2024 Cravecurb.com - All Rights Reserved</p>
+        </div>
+
+        
+  )
+}
+
+export default Footer
